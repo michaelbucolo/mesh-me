e5c6cdb2075436d17b0f73c06890407680d482fd
Michael Bucolo <michaelbucolomusic@gmail.com>
2026-04-30 20:48:33 -0500

    Remove public root loading fallback

--- src/app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { RouteLoadingPersonality } from "@/components/loading/route-loading-personality";
-
-export default function PublicLoading() {
-  return <RouteLoadingPersonality personality="public" surface="public" />;
-}
