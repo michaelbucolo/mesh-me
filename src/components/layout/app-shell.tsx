0154269ead51d8da36d51bb57d0ea46d0dfab462
michaelbucolo <michaelbucolomusic@gmail.com>
2026-09-17 18:51:24 -0500

    Make the admin console discoverable to administrators

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -338,10 +338,13 @@ function ShellTopBar({
               <Link href="/settings" className="mesh-dropdown-item mesh-account-item" style={{ ["--acc-i" as string]: 3 }}>Settings</Link>
               <Link href="/search" className="mesh-dropdown-item mesh-account-item lg:hidden" style={{ ["--acc-i" as string]: 4 }}>Search</Link>
               <Link href="/meshpro" className="mesh-dropdown-item mesh-account-item" style={{ ["--acc-i" as string]: 5 }}>MeshPro</Link>
+              {user.isAdmin && (
+                <Link href="/admin" data-feedback="navigate" className="mesh-dropdown-item mesh-account-item" style={{ ["--acc-i" as string]: 6 }}>Admin console</Link>
+              )}
               <button
                 type="button"
                 className="mesh-dropdown-item mesh-account-item w-full text-left"
-                style={{ ["--acc-i" as string]: 6 }}
+                style={{ ["--acc-i" as string]: 7 }}
                 onClick={(e) => {
                   (e.currentTarget.closest("details") as HTMLDetailsElement | null)?.removeAttribute("open");
                   window.dispatchEvent(new CustomEvent("mesh:open-bug-report"));
@@ -349,8 +352,8 @@ function ShellTopBar({
               >
                 Report a bug
               </button>
-              <hr className="mesh-account-item my-1 border-[var(--mesh-border)]" style={{ ["--acc-i" as string]: 7 }} />
-              <form action={signOut} className="mesh-account-item" style={{ ["--acc-i" as string]: 8 }}>
+              <hr className="mesh-account-item my-1 border-[var(--mesh-border)]" style={{ ["--acc-i" as string]: 8 }} />
+              <form action={signOut} className="mesh-account-item" style={{ ["--acc-i" as string]: 9 }}>
                 <button type="submit" className="mesh-dropdown-item mesh-dropdown-danger w-full text-left">
                   Sign out
                 </button>
